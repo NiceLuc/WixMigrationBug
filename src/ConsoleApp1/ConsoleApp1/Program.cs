7a12f87e09906136928e0a3dcc7acd2a5094ae59
Luc Hardbarger <luc.hardbarger@nice.com>
2024-12-03 12:14:25 -0500

    Creates a simple console app with a dependency on a library.

---
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new ClassLibrary1.Class1 {Name = "World" };
+            Console.WriteLine($"Hello {obj.Name}");
+            Console.ReadKey();
+        }
+    }
+}
